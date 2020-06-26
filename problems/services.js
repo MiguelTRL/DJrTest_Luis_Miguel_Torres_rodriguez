f17e8c33251b79544745608fb243598024521456
miguel <luisfb05@gmail.com>
2020-06-26 09:25:08 -0500

    update service

--- problems/services.js
+++ problems/services.js
@@ -10,7 +10,37 @@ function capitalizar(data){
     
 }
 
+
+
+function chat(message){
+
+
+    newData = message.split("");
+    var validate;
+        newData.map(item=> {
+
+
+            if(item == "h" || item =="H" ||
+             item == "o" || item =="O" ||
+             item == "l"|| item =="L" ||
+             item == "a" || item =="a" ){
+
+                validate = true;
+                
+             }else {
+
+                validate = false;
+
+
+             }
+
+    })
+
+    console.log("Message",validate);
+}
 functions.capitalizar = capitalizar;
+functions.chat = chat;
+
 
 module.exports = functions
 
