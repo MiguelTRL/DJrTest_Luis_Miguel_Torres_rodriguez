cb4dd09a95a74a0ea7c3806a0174b0a41f1d3021
miguel <luisfb05@gmail.com>
2020-06-26 11:35:00 -0500

    try again push project in proyecto

---
+++ proyecto/src/routes/index.js
@@ -0,0 +1,50 @@
+const {Router} = require('express');
+const router = Router()
+const admin =  require('firebase-admin')
+
+var serviceAccount = require("../../djrtestluismigueodriguez-firebase-adminsdk-r6fyx-596e076e15.json");
+
+
+admin.initializeApp({
+credential : admin.credential.cert(serviceAccount),
+databaseURL : 'https://djrtestluismigueodriguez.firebaseio.com/'
+
+});
+
+const db = admin.database()
+
+router.get('/' ,(req,res) => {
+
+db.ref('works').once('value',(snapshot)=> {
+    const data =snapshot.val();
+    res.render('index.hbs',{works : data });
+
+});
+
+router.get('/delete-work/:id',(req,res)=>  {
+
+    db.ref('works/'+ req.params.id).remove();
+    res.redirect('/')
+})
+
+
+
+
+    router.post('/newwork',(req,res)=> {
+    console.log(req.body)
+
+        const newWork = {
+
+            nameWork : req.body.name,
+            nameDescription : req.body.description
+
+        }
+            
+    db.ref('works').push(newWork);
+    res.redirect('/')
+
+    })
+
+}) 
+
+module.exports = router;
